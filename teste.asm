27d8553473ec14b0b1d32207bdb8faf2e058f4ac
Felipe <11felipe.caselli@gmail.com>
2024-08-13 12:05:50 -0300

    Test to fix fuleco move

--- teste.asm
+++ teste.asm
@@ -1,14 +1,4 @@
-.data
-.include "sprites/Mapas/arquivos .data/map1.data"		# inclui o .data com a imagem
-.include "sprites/fuleco/arquivos .data/fixedFuleco0.data"		# inclui o .data com a imagem
-.include "sprites/Mapas/arquivos .data/collisionmap1.data"
-
-
-
-.text
-	la s8, collisionmap1
-	la s9, map1
-	la s10, fixedFuleco0
+gameStart:
     # print map in frame 0
     	mv a0, s9
 	li a1, 0
@@ -31,6 +21,7 @@
 	
 	li s0, 0			# s0 = runing state : 0 = stop // 1 = left // 2 = right // 3 = up // 4 = down
 	li s7, 0
+	li t6, 0
 loopgame:
 	xori a3, a3, 1			# change frame 0 <--> 1
     # print map1
@@ -42,112 +33,93 @@ loopgame:
 	mv a1, s3
 	mv a2, s4
 	
+	li t6, 0
+getInput:	
+	li a0, 1
+	li a7, 32
+	ecall
+	
 	li t2, 0
 	li t1,0xFF200000		# load KDMMIO andress
 	lw t0,0(t1)			# Le bit de Controle Teclado
 	andi t0,t0,0x0001		# mascara o bit menos significativo
-	beq t0,zero, goLeft  		# Se nao ha tecla pressionada entao vai pula
+	beq t0,zero, clickLeft  	# Se nao ha tecla pressionada entao vai clickLeft
 	lw t2,4(t1)  			# le o valor da tecla tecla
 	sw t2,12(t1)  			# escreve a tecla pressionada no display
-	
-	
-clcikLeft:
-	li t1, 'a'
-	bne t2, t1, clickRight
-	li s7, 1
-clickRight:
-	li t1, 'd'
-	bne t2, t1, clickUp
-	li s7, 2
-clickUp:	
-	li t1, 'w'
-	bne t2, t1, clickDown
-	li s7, 3
-clickDown:
-	li t1, 's'
-	bne t2, t1, tryLeft
-	li s7, 4
-	
-tryLeft:
+	j clickLeft	
+		
+clickLeft:
 	mv a0, s8
 
-	li t1, 1
-	bne s7, t1, tryRight		# check if s7 = 1
+	li t1, 'a'
+	bne t2, t1, clickRight		# verifica se foi apertado a tecla 'a'
 	
 	addi a1, a1, -4			# update temporario da posicao para 4 pixels para esquerda
 	call checkCollision		# check da colisao superior esquerda
-	li t0, 0			# t0 = 0
 	addi a1, a1, 4			# cancela o update temporario do eixo X
-	beq t1, t0, goLeft		# se ha colisao, pula para "goLeft"
+	beqz t1, goLeft			# se ha colisao, pula para "goLeft"
 	
-	addi a1, a1, -4			# update da posicao para 4 pixels para esquerda
-	addi a2, a2, 12			# update da posicao para 15 pixels para baixo
-	call checkCollision		# check da colisao inferior esquerda
-	li t0, 0			# t0 = 0
+	addi a1, a1, -4			# update temporario da posicao para 4 pixels para esquerda
+	addi a2, a2, 12			# update temporario da posicao para 15 pixels para baixo
+	call checkCollision		# check da colisao superior esquerda
 	addi a1, a1, 4			# cancela o update temporario do eixo X
 	addi a2, a2, -12		# cancela o update temporario do eixo Y
-	beq t1, t0, goLeft		# se ha colisao, pula para "goLeft"
+	beqz t1, goLeft			# se ha colisao, pula para "goLeft"
 	
-	li s0, 1			# update s0 para 1
-	
-tryRight:
-	li t1, 2
-	bne s7, t1, tryUp		# check if s7 = 2
+	li s0, 1			# muda o s0 para 1
+clickRight:
+	li t1, 'd'
+	bne t2, t1, clickUp		# verirfica se foi apertado a tecla 'd'
 	
 	addi a1, a1, 16			# update temporario do eixo X para a direita 
 	call checkCollision		# check de colisao superior direita
 	addi a1, a1, -16		# cancela o update temporario do eixo X para a direita
 	li t0, 0			# t0 = 0
-	beq t1, t0, goLeft		# se ha colisao, pula para "goLeft"
+	beqz t1, goLeft			# se ha colisao, pula para "goLeft"
 	
-	addi a1, a1, 12			# update temporario do eixo X para a diereita
+	addi a1, a1, 16			# update temporario do eixo X para a diereita
 	addi a2, a2, 12			# update temporario do eixo Y para baixo
 	call checkCollision		# check de colisao inferior direita
-	addi a1, a1, -12		# cancela o update temporario do eixo X para a direita
-	addi a2, a2, -12		# cancela o update temporario do eixo Y para a esquerda
-	li t0, 0			# t0 = 0
-	beq t1, t0, goLeft		# se ha colisao, pula para "goLeft"
+	addi a1, a1, -16		# cancela o update temporario do eixo X para a direita
+	addi a2, a2, -12		# cancela o update temporario do eixo Y para a esquerda	
+	beqz t1, goLeft			# se ha colisao, pula para "goLeft"
 	
-	li s0, 2			# update s0 para 2
-tryUp:	
-	li t1, 3
-	bne s7, t1, tryDown		# check if s0 = 3
+	li s0, 2
+clickUp:	
+	li t1, 'w'
+	bne t2, t1, clickDown		# verifica se foi apertado a tecla 'w'
 	
 	addi a2, a2, -4			# update temporario do eixo Y para cima
-	call checkCollision		# check de colisao superior equerda
+	call checkCollision		# update de colisao superior esquerda
 	addi a2, a2, 4			# cancela o update temporario do eixo Y
-	li t0, 0			# t0 = 0
-	beq t1, t0, goLeft		# se ha colisao, pula para "goLeft"
+	beqz t1, goLeft			# se ha colisao, pula para "goLeft"
 	
 	addi a2, a2, -4			# update temporario do eixo Y para cima
-	addi a1, a1, 12			# update temporario do eixo X para direita
-	call checkCollision		# check de colisao superior direito
-	addi a2, a2, 4			# cancela update temporario do eixo Y para cima
-	addi a1, a1, -12		# cancela update temporario do eixo X para direita
-	li t0, 0			# t0 = 0
-	beq t0, t1, goLeft		# se ha colisao, pula para "goLeft"
-	
-	li s0, 3			# update s0 para 3
-tryDown:
-	li t1, 4
-	bne s7, t1, goLeft		# check if s0 = 4
-	
-	addi a2, a2, 16			# update temporario do eixo Y para baixo
-	call checkCollision		# check de colisao inferior equerda
-	addi a2, a2, -16			# cancela o update temporario do eixo Y
-	li t0, 0			# t0 = 0
-	beq t1, t0, goLeft		# se ha colisao, pula para "goLeft"
-	
-	addi a2, a2, 16			# update temporario do eixo Y para baixo
-	addi a1, a1, 12			# update temporario do eixo X para direita
-	call checkCollision		# check de colisao superior direito
-	addi a2, a2, -16		# cancela o update temporario do eixo Y 
+	addi a1, a1, 12			# update temporario do eixo X para cima
+	call checkCollision		# update de colisao superior esquerda
+	addi a2, a2, 4			# cancela o update temporario do eixo Y
 	addi a1, a1, -12		# cancela o update temporario do eixo X
-	li t0, 0			# t0 = 0
-	beq t0, t1, goLeft		# se ha colisao, pula para "goLeft"
+	beqz t1, goLeft			# se ha colisao, pula para "goLeft"
 	
-	li s0, 4			# update s0 para 4
-
+	li s0, 3
+clickDown:
+	li t1, 's'
+	bne t2, t1, goLeft
+	
+	addi a2, a2, 16			# update temporario do eixo Y para baixo
+	call checkCollision		# update de colisao superior esquerda
+	addi a2, a2, -16		# cancela o update temporario do eixo Y
+	beqz t1, goLeft			# se ha colisao, pula para "goLeft"
+	
+	addi a2, a2, 16			# update temporario do eixo Y para baixo
+	addi a1, a1, 12			# update temporario do eixo X para cima
+	call checkCollision		# update de colisao superior esquerda
+	addi a2, a2, -16		# cancela o update temporario do eixo Y
+	addi a1, a1, -12		# cancela o update temporario do eixo X
+	beqz t1, goLeft			# se ha colisao, pula para "goLeft"
+	
+	li s0, 4
+	
 goLeft:
 	mv a0, s8
 
@@ -255,16 +227,10 @@ dontTeleportRight:
 	
 	j loopgame
 	
-	
-	
-	
-	
-	
-	
-	
 		
 	li a7, 10
 	#ecall
+	ret
 	
 	
 	
