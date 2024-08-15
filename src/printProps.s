1a4d97abcef85ce2fc685ad472bf70df098e567a
Felipe Caselli <11felipe.caselli@gmail.com>
2024-08-15 11:40:56 -0300

    comments in printProps

--- src/printProps.s
+++ src/printProps.s
@@ -3,49 +3,43 @@
 	#	a0 = endereço colisao			#
 	#	a1 = x					#
 	#	a2 = y					#
-	#	a3 = frame (0 ou 1)			#
-	#################################################
-	#	t0 = endereco do bitfundo display	#
-	#	t1 = endereco da imagem			#
-	#	t2 = contador de linha			#
-	# 	t3 = contador de coluna			#
-	#	t4 = largura				#
-	#	t5 = altura				#
 	#################################################
 
 printProps:	
-	mv a7, a0
-	
+	mv a7, a0			# salva o endereco do mapa de colisoes no a7
 	
 loopPrintProps:
-	li t0, 320
-	li t1, 240
+	li t0, 320			# t0 = 320	
 	
-	add t2, a7, a1
-	mul t3, a2, t0
-	add t2, t2, t3
+	add t2, a7, a1			# t2 = endereco + a1
+	mul t3, a2, t0			# t3 = a2*320
+	add t2, t2, t3			# t2 = t2 + t3 = endereco + a1 + a2*320
 	
-	lw t3, 0(t2)
-	srli t3, t3, 24
-	
-	li t4, 7
-	bne t3, t4, jDot
-	la a0, dot
-	call print
-	li t3, 7
+	lw t3, 0(t2)			# t3 = word no endereco t2
+	srli t3, t3, 24			# pega so os 8 bits da esquerda
+		
+	li t4, 7			# t4 = 7
+	bne t3, t4, jDot		# se t3 != t4, pula para "jDot"
+	addi a1, a1, -4			# corrige a posicao X para a esquerda (bug do print)
+	la a0, dot			# a0 = endereco do pontinho
+	call print			# printa pontinho
+	addi a1, a1, 4			# volta a posicao X para a original
+	li t3, 7			# volta o t3 para 7 (pra ter a outra verificacao)
 jDot:
-	li t4, 192
-	bne t3, t4, jBrazuca
-	la a0, brazuca
-	call print
+	li t4, 192			# t4 = 192
+	bne t3, t4, jBrazuca		# se t3 != t4, pula para "jBrazuca"
+	addi a1, a1, -4			# corrige a posicao X para a esquerda (bug do print)
+	la a0, brazuca			# a0 = endereco da brazuca
+	call print			# printa brazuca
+	addi a1, a1, 4			# volta a posicao X para a original
 jBrazuca:
-	li t0, 320
-	li t1, 240
-	
-	addi a1, a1, 1
-	bne a1, t0, loopPrintProps
-	li a1, 0
-	addi a2, a2, 1
-	bne a2, t1, loopPrintProps
+	li t0, 320			# t0 = 320
+	li t1, 240			# t1 = 240
+		
+	addi a1, a1, 1			# a1 += 1	
+	bne a1, t0, loopPrintProps	# se a1 != 320, pula para "loopPrintProps"
+	li a1, 0			# a1 = 0
+	addi a2, a2, 1			# a2 += 1
+	bne a2, t1, loopPrintProps	# se a2 != 240, pula para "loopPrintProps"
 	
 	j input		# retorna
