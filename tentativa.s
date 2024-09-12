a919cba946b3aa14e1b2aabc5cf32cbcd00c4fa4
Felipe Caselli <11felipe.caselli@gmail.com>
2024-09-12 19:19:48 -0300

    Fix: fix test

--- tentativa.s
+++ tentativa.s
@@ -3,23 +3,25 @@
 SCR: .string  	 "SCORE:"
 
 .text
-
-	la a0,SCR  	#salva o endereço da string
+loop:
+	la a0,SCR  	#salva o endereï¿½o da string
 	li a1,10        #pixel da coluna
-	li a2,10	#pixel da linha
+	li a2,5		#pixel da linha
 	li a3,0x00FF	#2 primeiros:cor do fundo  2 ultimos:cor da palavra
 	li a4,0		#frame
 	li a7,104	#104: print string
 	ecall
 	
-	li a0,0		#imprimir o score
+	li a0,40		#imprimir o score
 	li a1,60
-	li a2, 10
+	li a2, 5
 	li a3, 0x00FF
 	li a4,0
 	li a7,101	#101: print int
 	ecall
-
+	
+	j loop
+	
 	li a7,10
 	ecall
 
